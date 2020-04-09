57cd0ab6a14b10f79750fad592c10e7c78cba31e
Alejandro Peinó <alepeino@gmail.com>
2020-04-09 19:18:01 -0300

    Initial config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js'
+  },
+  devtool: isDevelopment && 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    })
+  ]
+}
